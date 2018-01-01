60ea26612cf8b58b65edd3fb7cb72f2819548793
savdevarney <savannah.devarney@gmail.com>
2017-12-31 16:51:17 -0800

    started Angular with Mosh course - notes up to Reactive Forms and worked through template forms and examples in other course

--- Angular/projects/angularapp/src/app/components/sandbox/sandbox.component.ts
+++ Angular/projects/angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -3,42 +3,45 @@ import { Component } from '@angular/core';
 @Component({
 	selector:'sandbox',
 	template:`
-		<h1>Hello World </h1>
+		<h1>Hello World</h1>
+		<form (submit)="onSubmit()">
+			<div class="form-group">
+				<label>Name</label>
+				<input type="text" class="form-control" [(ngModel)]="name" name="name" #a #b="ngModel" (change)="viewModel(b); viewElement(a)">
 
-		<div>
-			<input type="text" (keyup)="fireEvent($event)" placeholder="keyup event">
-		</div>
-
-		<div>
-			<input type="text" (keydown)="fireEvent($event)" placeholder="keydown event">
-		</div>
-
-		<div>
-			<input type="text" (focus)="fireEvent($event)" placeholder="focus event">
-		</div>
-
-		<div>
-			<input type="text" (blur)="fireEvent($event)" placeholder="blur event">
-		</div>
-
+			</div>
+			<input type="submit" class="btn btn-success" value="Submit">
+		</form>
 		<hr>
-
-		<div>
-			<input type="text" (keyup)="changeText($event)" placeholder="change text" value={{text}}>
-		</div>
-
-		<h2>{{ text }}</h2>
+		<ul class="list-group">
+			<li class="list-group-item" *ngFor="let user of users">{{ user }}</li>
+		</ul>
 
 	`
 })
 
 export class SandboxComponent {
-	text:string = 'Hello';
-	fireEvent(e) {
-		console.log(e.type);
-	}
-	changeText(e) {
-		this.text = e.target.value;
+	name:string = '';
+	users:string[] = ['John Doe', 'Mary Swanson', 'Kevin Smith'];
+
+	onSubmit() {
+		console.log(this.name);
+		this.users.push(this.name);
+		this.name = '';
 	}
 
+	viewModel(model) {
+		console.log(model);
+	}
+
+	viewElement(element) {
+		console.log(element);
+	}
 }
+
+
+// element: 
+// <input class="form-control ng-valid ng-dirty ng-touched" name="name" type="text" ng-reflect-name="name" ng-reflect-model="">
+
+// ngModel of element: 
+// NgModel {_parent: NgForm, name: "name", valueAccessor: DefaultValueAccessor, _rawValidators: Array(0), _rawAsyncValidators: Array(0), …}asyncValidator: nullcontrol: FormControl {validator: null, asyncValidator: null, pristine: false, touched: true, _onCollectionChange: ƒ, …}dirty: (...)disabled: (...)enabled: (...)errors: (...)formDirective: (...)invalid: (...)model: ""name: "name"path: (...)pending: (...)pristine: (...)status: (...)statusChanges: (...)touched: (...)untouched: (...)update: EventEmitter {_isScalar: false, observers: Array(1), closed: false, isStopped: false, hasError: false, …}valid: (...)validator: (...)value: (...)valueAccessor: DefaultValueAccessor {_renderer: DebugRenderer2, _elementRef: ElementRef, _compositionMode: true, onChange: ƒ, onTouched: ƒ, …}valueChanges: (...)viewModel: ""_parent: NgForm {submitted: true, _directives: Array(1), ngSubmit: EventEmitter, form: FormGroup}_rawAsyncValidators: []_rawValidators: []_registered: true__proto__: NgControl
